1873b0d7314b539c47fb006dc7aa28ebcc2bbdfd
Mariusz Zalewski <mariusz.zalewski@tty.host>
2021-12-19 12:16:45 +0100

    multiple monitor setup

--- launch.sh
+++ launch.sh
@@ -7,6 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using theme config
-polybar mybar -c ~/.config/polybar/$THEME.config &
+# polybar mybar -c ~/.config/polybar/$THEME.config &
+
+# Launch polybars for every connected monitor
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar mybar -c ~/.config/polybar/$THEME.config &
+  done
+else
+  polybar mybar -c ~/.config/polybar/$THEME.config &
+fi
 
 echo "Polybar launched..."
